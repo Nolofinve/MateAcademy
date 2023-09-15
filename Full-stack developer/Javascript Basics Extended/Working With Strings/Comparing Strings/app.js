1599cad278b69271c377f54a1973540954e85766
Vasyl Dutkovskyi <festuuu@gmai.com>
2023-09-15 16:09:52 +0300

    15.09.2023 (2)

--- Full-stack developer/Javascript Basics Extended/Working With Strings/Comparing Strings/app.js
+++ Full-stack developer/Javascript Basics Extended/Working With Strings/Comparing Strings/app.js
@@ -1,22 +1,15 @@
 'use strict';
 
-const x1 = 42; // decimal
+const name1 = 'Hedgehog';
+const name2 = 'apple';
 
-const x2 = 0b101010; // binary
-const x3 = 0o52; // octal
-const x4 = 0x2A; // hexadecimal
-const x5 = 0x2a; // hexadecimal
+console.log(
+  name1.toLowerCase() > name2.toLocaleLowerCase()
+);
 
-console.log(x1, x2, x3, x4, x5);
+const word1 = 'їжачок';
+const word2 = 'яблуко';
 
-let y1 = 42.000;
-let y2 = 42.;
-
-// Scientific notation
-
-let y3 = 0.42e2; // 0.42 * (10 ** 2) === 42
-let y4 = .42e2;
-
-let y5 = 42000e-3; // 4200 / (10 ** 3) === 42
-
-console.log(y1, y2, y3, y4, y5);
+console.log(
+  word1.localeCompare(word2) > 0
+);
